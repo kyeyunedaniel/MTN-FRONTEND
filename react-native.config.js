1645c37d78771afd6c69715c5dd44d9c8145f2f9
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2026-07-03 11:30:04 +0300

    updated changes

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./src/assets/fonts'],
+//   /Users/kyeyunedaniel/Desktop/untitled folder 2/AwesomeProject/src/assets/fonts/Irish_Grover/IrishGrover-Regular.ttf
+};
