d8ed8242a6e285b3ed552f40dbcd2c373527f5c7
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2026-07-06 11:08:17 +0300

    adding bootsplash

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/components/Navigator/navigator';
-
-
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({fade: true});
+  }, []);
+
   return (
     <NavigationContainer>
       <AppNavigator />
